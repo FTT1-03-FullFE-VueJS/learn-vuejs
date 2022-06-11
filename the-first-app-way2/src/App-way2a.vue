b9ffd08c724e8d30a5fdfc4d6a1d29e129be6fa1
penry28 <phamdinhhung28.it@gmail.com>
2022-06-11 20:34:20 +0700

    [Added] way2,3 to code vuejs

---
+++ the-first-app-way2/src/App-way2a.vue
@@ -0,0 +1,38 @@
+<!-- HTML -->
+<template>
+  <div class="app-container">
+    <h1>{{ title }}</h1>
+    <button @click="minus">-</button>
+    <input type="text" :value="number">
+    <button @click="plus">+</button>
+    <br>
+    <button @click="changeTitle('Today i very happy!')">Change title</button>
+  </div>
+</template>
+
+<!-- JAVASCIPT -->
+<script setup>
+import { ref } from 'vue';
+
+let title = ref('Counter number application');
+const number = ref(1);
+
+function changeTitle(titleChanged) {
+  title.value = titleChanged;
+}
+
+function plus() {
+  number.value++;
+}
+
+function minus() {
+  number.value--;
+}
+</script>
+
+<!-- CSS -->
+<style>
+.app-container {
+  color: #f00;
+}
+</style>
---
+++ the-first-app-way2/src/App.vue
@@ -0,0 +1,55 @@
+<!-- HTML -->
+<template>
+  <div class="app-container">
+    <h1>{{ title }}</h1>
+    <button @click="minus">-</button>
+    <input type="text" :value="number">
+    <button @click="plus">+</button>
+    <br>
+    <button @click="changeTitle('Today i very happy!')">Change title</button>
+  </div>
+</template>
+
+<!-- JAVASCIPT -->
+<script>
+import { ref } from 'vue';
+
+export default {
+  data() {
+    return {
+      title: 'Title 2',
+    };
+  },
+  setup() {
+    let title = ref('Counter number application');
+    const number = ref(1);
+
+    function changeTitle(titleChanged) {
+      title.value = titleChanged;
+    }
+
+    function plus() {
+      number.value++;
+    }
+
+    function minus() {
+      number.value--;
+    }
+
+    return {
+      title,
+      number,
+      plus,
+      minus,
+      changeTitle
+    };
+  },
+}
+</script>
+
+<!-- CSS -->
+<style>
+.app-container {
+  color: #f00;
+}
+</style>
