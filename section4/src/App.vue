acfb6fcac65f37ef7494dc75d56a6c71b19ddb83
penry28 <phamdinhhung28.it@gmail.com>
2022-06-18 10:48:20 +0700

    [Done] section4

---
+++ section4/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="container">
+    <button @click="counter++">{{counter}}</button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUpdated, ref, reactive, onUnmounted, onActivated } from 'vue';
+
+// const counter = ref(1);
+
+// onUpdated(() => {
+//   // console.log('on updated ran!');
+// });
+
+// onActivated(() => {
+//   console.log('called on initial mount');
+// });
+
+// onMounted(function() {
+//   const containerEl = document.querySelector('#container');
+//   console.log(containerEl);
+// });
+
+// onMounted(() => {
+//   const containerEl = document.querySelector('#container');
+//   console.log(containerEl);
+// });
+
+// onUnmounted(() => {
+//   // console.log('Vue die');
+// });
+</script>
